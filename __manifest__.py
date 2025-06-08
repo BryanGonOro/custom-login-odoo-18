3b20e538f320715865bbfaae18e82a3bab8c2591
Bryan Gonzalez Orostegui <62038882+BryanGonOro@users.noreply.github.com>
2025-06-08 14:57:11 -0500

    xml actualizado

--- __manifest__.py
+++ __manifest__.py
@@ -4,7 +4,10 @@
     "author": "DOWE SAS",
     "category": "Customization",
     "depends": ["web"],
-    "data": ["views/login_template.xml"],
+    "data": [
+        "views/login_template.xml",
+        "views/add_message.xml"
+    ],
     "installable": True,
     "auto_install": False,
 }
