78366d01d7ced1406008f48539946ac25a9d6ebb
Bryan Gonzalez Orostegui <62038882+BryanGonOro@users.noreply.github.com>
2025-06-08 13:15:16 -0500

    custom login

---
+++ __manifest__.py
@@ -0,0 +1,10 @@
+{
+    "name": "Custom Login Page",
+    "version": "1.0",
+    "author": "SGV Digital",
+    "category": "Customization",
+    "depends": ["web"],
+    "data": ["views/login_template.xml"],
+    "installable": True,
+    "auto_install": False,
+}
