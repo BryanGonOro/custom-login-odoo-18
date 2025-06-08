c2228a8a005f9f4fd2e56b09cb3fd398c14b0876
Bryan Gonzalez Orostegui <62038882+BryanGonOro@users.noreply.github.com>
2025-06-08 14:22:42 -0500

    xml actualizado

--- __manifest__.py
+++ __manifest__.py
@@ -1,7 +1,7 @@
 {
     "name": "Custom Login Page",
     "version": "1.0",
-    "author": "SGV Digital",
+    "author": "DOWE SAS",
     "category": "Customization",
     "depends": ["web"],
     "data": ["views/login_template.xml"],
